7a2f018097d1019bc90965793cddcfe2e09fa7b4
paulohtolotti <paulohtolotti@alumni.usp.br>
2026-09-14 17:10:02 -0300

    feat: exercicio 1 do capitulo 2

--- src/chapter2/exercises.py
+++ src/chapter2/exercises.py
@@ -1,2 +1,17 @@
+from sqlalchemy import select
+
 from src.db import Session
 from src.models import Product
+
+
+def exercise1():
+    with Session() as session, session.begin():
+        query = select(Product).order_by(Product.name.asc()).where(Product.year == 1983).limit(3)
+        res = session.scalars(query).all()
+        for idx, r in enumerate(res, start=1):
+            print(f"#{idx}: {r}")
+
+
+if __name__ == "__main__":
+    exercise1()
+
